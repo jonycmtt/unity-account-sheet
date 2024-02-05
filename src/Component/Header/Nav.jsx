fbfbbe5af63cd34264f7dd183b91df74a34c5732
muhammadmasud1 <mdmasudrana15020@gmail.com>
2024-02-05 19:12:48 +0600

    new prject start

---
+++ src/Component/Header/Nav.jsx
@@ -0,0 +1,10 @@
+
+const Nav = () => {
+              return (
+                            <div>
+                               <h1>This is navbar</h1>           
+                            </div>
+              );
+};
+
+export default Nav;
